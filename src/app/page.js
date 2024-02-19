d1397579c74d2f212489d70bc1292711ead520bd
Anas Ali Khan <105873373+Lucifier150@users.noreply.github.com>
2024-02-20 04:01:31 +0500

    taking care of eslint warnings

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Herosection from "@/app/components/Herosection";
 const Page = () => {
     return (
         <>
-            <Herosection title={"LET'S WATCH MOVIE TOGETHER"} imageUrl={'/home.svg'} contentText={"From award-winning dramas to blockbuster action movies, we've got you covered. Browse our selection of the latest and greatest movies, and find your new favorite today."}/>
+            <Herosection title={"LET\'S WATCH MOVIE TOGETHER"} imageUrl={'/home.svg'} contentText={"From award-winning dramas to blockbuster action movies, we've got you covered. Browse our selection of the latest and greatest movies, and find your new favorite today."}/>
         </>
     );
 };
